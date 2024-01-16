be355f81eede268777af2a106583e97436171df0
shimon@dotrothischild.com <shimon@dotrothschild.com>
2024-01-16 18:00:09 +0200

    shows location on second run, not first, trying to find reason why not after permission given.

--- app/src/main/java/com/inzhood/library/kotlingpsdotrothschild/MainActivity.kt
+++ app/src/main/java/com/inzhood/library/kotlingpsdotrothschild/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.inzhood.library.kotlingpsdotrothschild
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.location.Location
+import android.location.LocationProvider
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,8 +21,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -27,25 +32,39 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.inzhood.library.gpslibrary.hasPermission
 import com.inzhood.library.gpslibrary.model.TransportSpeeds
 import com.inzhood.library.gpslibrary.route.RouteUI
 import com.inzhood.library.kotlingpsdotrothschild.ui.theme.KotlinGpsDotrothschildTheme
 
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: MainActivityViewModel by viewModels()
+   // no factory private val viewModel: MainActivityViewModel by viewModels()
+   private val viewModel: MainActivityViewModel by viewModels() {
+       MainActivityViewModelFactory(this.application)
+   }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel.getLastKnownLocation()
+        viewModel.startUpdatingLocation()
         viewModel.updateCandidateSpeed(getString(R.string.touch_and_select_from_list)) // Modify to view model factory when getting complex
         setContent {
             MaterialTheme {
                 GpsSelectionScreen(viewModel)
             }
-
-
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        if (!hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+        }
+
     }
 }
 
@@ -62,8 +81,7 @@ fun GpsSelectionScreen(viewModel: MainActivityViewModel)
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            //GpsSelectionScreen(viewModel)
-            // }
+            LocationDisplay(viewModel) // Add the LocationDisplay composable
 
             Spacer(modifier = Modifier.height(16.dp))
             val chooseSelectionText = stringResource(R.string.touch_and_select_from_list)
@@ -162,9 +180,29 @@ fun GpsSelectionScreen(viewModel: MainActivityViewModel)
 
 } // end of GpsSelectionScreen
 
+
+@Composable
+fun LocationDisplay(viewModel: MainActivityViewModel) {
+    val location by viewModel.location.observeAsState()
+
+    LaunchedEffect(true) {
+        viewModel.getLastKnownLocation()
+        viewModel.startUpdatingLocation()
+    }
+
+    Text(
+        text = location?.let { showLocation(it) } ?: stringResource(R.string.no_location),
+        modifier = Modifier.semantics { contentDescription = "Location" }
+    )// Accessibility
+}
+
+fun showLocation(location: Location): String {
+    return "${location.latitude}, ${location.longitude}"
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GpsSelectionScreenPreview() { // need this
-    GpsSelectionScreen(MainActivityViewModel())
+    val context = LocalContext.current
+    GpsSelectionScreen(MainActivityViewModel(context))
 }
-
--- app/src/main/java/com/inzhood/library/kotlingpsdotrothschild/MainActivityViewModel.kt
+++ app/src/main/java/com/inzhood/library/kotlingpsdotrothschild/MainActivityViewModel.kt
@@ -1,13 +1,58 @@
 package com.inzhood.library.kotlingpsdotrothschild
 
+import android.content.Context
+import android.location.Location
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.location.LocationServices
+import com.inzhood.library.gpslibrary.awaitLastLocation
+import com.inzhood.library.gpslibrary.locationFlow
+import com.inzhood.library.gpslibrary.route.RouteLogic
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.launch
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(context: Context) : ViewModel() {
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+    private val _location = MutableLiveData<Location>()
+    val location: LiveData<Location> = _location
+    fun getLastKnownLocation() {
+        viewModelScope.launch {
+            try {
+                val lastLocation = fusedLocationClient.awaitLastLocation()
+                _location.value = lastLocation // Update LiveData with last known location
+            } catch (e: Exception) {
+                Log.e("GpsLocationViewModel", "Unable to get last known location", e)
+                // TODO:  Consider notifying the UI about the error
+            }
+        }
+    }
+    fun startUpdatingLocation() {
+        viewModelScope.launch { // Use viewModelScope for lifecycle-aware coroutines
+            fusedLocationClient.locationFlow()
+                .conflate()
+                .catch { e ->
+                    Log.e("GpsLocationViewModel", "Unable to get location", e)
+                    // TODO: notifying the UI about the error
+                }
+                .collect { location ->
+                    RouteLogic.addLocation(location)
+                    _location.value = location
+                }
+        }
+    }
 
+
+
+    //  ******************  new code ****************
     private val count: MutableState<Int> = mutableIntStateOf(0)
     fun increment() {
         count.value++
@@ -16,10 +61,10 @@ class MainActivityViewModel : ViewModel() {
 
     private val _currentLocation =
         MutableStateFlow("placeholder location") // THis string is initial value
-    val currentLocation: StateFlow<String> = _currentLocation
+    val currentLocation: StateFlow<String> =  _currentLocation
 
     private fun updateLocation() {
-        _currentLocation.value = count.value.toString()
+        _currentLocation.value = _location.value.toString() // for testing only count.value.toString()
     }
 
     // The currently displayed candidate speed, accepted on button click
@@ -37,6 +82,15 @@ class MainActivityViewModel : ViewModel() {
     fun updateCurrentSpeed() {
         _currentSpeed.value = _candidateSpeed.value
     }
+}
 
-
+// *************************************   FACTORY  ********************************************
+class MainActivityViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainActivityViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainActivityViewModel(context) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
